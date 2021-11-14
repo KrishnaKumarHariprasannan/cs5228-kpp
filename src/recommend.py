cf9381c91c3b1e55e44869f6cd79971525735c2c
Rishabh Sheoran <sheoran95@gmail.com>
2021-11-14 17:51:48 +0530

    Changes in recommendation system user-item

--- src/recommend.py
+++ src/recommend.py
@@ -21,14 +21,17 @@ def get_user_recoms(price, depreciation, type_of_vehicle, top_n):
         user_pref_dict.pop('type_of_vehicle')
     # print(user_pref_dict)
     user_pref_cols = get_user_pref_cols(user_pref_dict)
+    # print('user_pref_cols: ', user_pref_cols)
     df_train_user_pref_cols = train.loc[:, ['listing_id']+user_pref_cols]
-    df_prepared_num = prepare_df_num(df_train_user_pref_cols, user_pref_dict, pref_dict_num)
-    user_row_dict = prepare_user_row_dict(df_prepared_num, user_pref_dict, pref_dict_num, pref_dict_cat)
-    df_prepared_normalized = normalize_df(df_prepared_num, user_pref_cols, pref_dict_num)
-    df_prepared_normalized = prepare_df_cat(df_prepared_normalized, user_pref_dict, pref_dict_cat)
-    df_user_row, cols_to_be_dropped = get_user_row(user_row_dict, pref_dict_num, pref_dict_cat, df_prepared_normalized)
-    df_prepared_final = df_prepared_normalized.drop(cols_to_be_dropped, axis=1)
+    df_to_be_one_hot = prepare_df_ranges(df_train_user_pref_cols, user_pref_dict)
+    df_one_hot = prepare_df_one_hot(df_to_be_one_hot, user_pref_dict, my_dict)
+    df_user_row = prepare_df_user_row(df_one_hot, my_dict, user_pref_dict)
+    cols_to_be_dropped = []
+    for col in user_pref_dict:
+        if col in my_dict:
+            cols_to_be_dropped.append(col)
     df_user_row_final = df_user_row.drop(cols_to_be_dropped, axis=1)
+    df_prepared_final = df_one_hot.drop(cols_to_be_dropped, axis=1)
 
     x_user = df_user_row_final['listing_id'].to_numpy()
     y_user = df_user_row_final.iloc[:,1:].to_numpy()
@@ -40,7 +43,6 @@ def get_user_recoms(price, depreciation, type_of_vehicle, top_n):
     ind_ordered = np.argsort(similarity_user_item)[::-1]
     df_top_user_item = get_top_recommendations_user_item(similarity_user_item, ind_ordered, x_items, y_items, top_n, train)
     # print('user_pref_dict: ', user_pref_dict)
-    # return df_top_user_item.loc[:, ['listing_id', 'make', 'model', 'depreciation', 'price', 'type_of_vehicle']]
     return df_top_user_item
 
 def get_similar_items(listing_id_chosen, top_n=10):
@@ -63,7 +65,4 @@ def get_similar_items(listing_id_chosen, top_n=10):
     indices_ii_ordered = np.argsort(similarity_item_item)[::-1]
     df_top_ii = get_top_recommendations_ii(similarity_item_item, indices_ii_ordered, x_items_ii, y_items_ii, top_n+1, train)
     df_top_ii = df_top_ii[df_top_ii.listing_id != listing_id_chosen]
-    # df_top_ii = df_top_ii[df_top_ii.listing_id != listing_id_chosen]
-    # return df_top_ii.loc[:, ['listing_id','make', 'model', 'vehicle_age', 'type_of_vehicle', 'depreciation',
-    #                'dereg_value', 'mileage', 'price', 'engine_cap',  'fuel_type','transmission', 'brand_rank']]
     return df_top_ii
--- src/user_item_funcs.py
+++ src/user_item_funcs.py
@@ -17,130 +17,94 @@ depreciation_bins = [0, 10000, 11000, 12000, 13000, 14000, 16000, 18000, 20000, 
 
 type_of_vehicle_ranges = ['sports car', 'luxury sedan', 'suv', 'hatchback','mid-sized sedan', 'stationwagon',
                           'mpv', 'bus/mini bus', 'truck','others', 'van']
-pref_dict_num = {'price':{"ranges":price_ranges, 'bins':price_bins}, 'depreciation':{"ranges":depreciation_ranges,
-                                                                                    'bins':depreciation_bins}}
-pref_dict_cat = {'type_of_vehicle': type_of_vehicle_ranges}
+my_dict = {'price':{'ranges':price_ranges, 'bins':price_bins},
+           'depreciation':{'ranges':depreciation_ranges, 'bins':depreciation_bins}}
+
+def prepare_df_ranges(df_train_user_pref_cols, user_pref_dict):
+    df = df_train_user_pref_cols.copy()
+    for col, val_range in user_pref_dict.items():
+        if col in my_dict:
+            pipe = Pipeline(
+                            [
+                                (
+                                    col+'_range',
+                                    ColumnValuesToCategory(
+                                        col,
+                                        col+"_range",
+                                        my_dict[col]['bins'],
+                                        my_dict[col]['ranges'],
+                                    ),
+                                )
+                            ]
+                    )
+
+            df = pipe.fit_transform(df)
+    return df
 
 def get_user_pref_cols(user_pref_dict):
     cols = list(user_pref_dict.keys())
     return cols
 
-def prepare_df_num(df_train_user_pref_cols, user_pref_dict, pref_dict_num):
-    df = df_train_user_pref_cols.copy()
+def prepare_df_one_hot(df_to_be_one_hot, user_pref_dict, my_dict):
+    df = df_to_be_one_hot.copy()
     for col, val_range in user_pref_dict.items():
-        if col in pref_dict_num:
-            pipe = Pipeline(
-                [
-                    (
-                        col+'_range',
-                        ColumnValuesToCategory(
-                            col,
-                            col+"_range",
-                            pref_dict_num[col]['bins'],
-                            pref_dict_num[col]['ranges'],
-                        ),
-                    )
-                ]
-            )
-
-            df = pipe.fit_transform(df)
-
-    return df
-
-def prepare_df_cat(df_prepared_num, user_pref_dict, pref_dict_cat):
-    df = df_prepared_num.copy()
-    for col, val_range in user_pref_dict.items():
-        if col in pref_dict_cat:
-#             print('col: ', col)
+        if col in my_dict:
+#             df = df.drop(col, axis=1)
+            df = pd.get_dummies(df, columns = [col+'_range'], prefix=col, prefix_sep='_')
+        else:
             df = pd.get_dummies(df, columns = [col], prefix='', prefix_sep='')
-
     return df
-
-def normalize_df(df_prepared_num, user_pref_cols, pref_dict_num):
-    df_prep = df_prepared_num.copy()
-    cols = []
-    for col in user_pref_cols:
-        if col in pref_dict_num:
-            cols.append(col)
-    df = df_prep.loc[:, cols]
-    df_normalized = (df - df.min()) / (df.max() - df.min())
-    df_prep.loc[:, cols] = df_normalized
-    return df_prep
-
-def get_col_value(val_range, pref_dict_num, col):
-    ranges = pref_dict_num[col]['ranges']
-    bins = pref_dict_num[col]['bins']
-    index_ranges = ranges.index(val_range)
-#     print('val_range: ', val_range)
-#     print('ranges: ', ranges)
-#     print('index_ranges: ', index_ranges)
-    if index_ranges == len(bins)-2:
-        value = 2*bins[index_ranges]-bins[index_ranges-1]
-    else:
-        value = (bins[index_ranges] + bins[index_ranges+1]) / 2
-    return value
-
-def prepare_user_row_dict(df_prepared_num, user_pref_dict, pref_dict_num, pref_dict_cat):
-    row_dict ={}
-    row_dict['listing_id'] = 0
-    for col, val_range in user_pref_dict.items():
-        if col in pref_dict_num:
-            col_max = df_prepared_num.loc[:, col].max()
-            col_min = df_prepared_num.loc[:, col].min()
-            col_val = get_col_value(val_range, pref_dict_num, col)
-            col_val_normalized = (col_val - col_min) / (col_max - col_min)
-            row_dict[col] = [col_val_normalized]
-        elif col in pref_dict_cat:
-            row_dict[col] = [val_range]
-#     print('row_dict: ', row_dict)
-    return row_dict
-
-def get_user_row(user_row_dict, pref_dict_num, pref_dict_cat, df_prepared_normalized):
-    user_row ={}
-    for col, val in user_row_dict.items():
-        if col in pref_dict_num:
-            user_row[col] = val[0]
-        elif col in pref_dict_cat:
-            for v in val:
-                user_row[v] = 1
-    # print(user_row)
-    df_user_row = pd.DataFrame(columns = df_prepared_normalized.columns)
-    # print(df_user_row.dtypes)
-    for new_col in df_user_row.columns:
-        if new_col not in user_row:
-            user_row[new_col] = 0
-
-    df_user_row = df_user_row.append(user_row,ignore_index=True)
-
-    convert_dtype_dict = {}
-    cols_to_be_dropped = []
-    for col in df_user_row.columns:
-        convert_dtype_dict[col] = str(df_prepared_normalized[col].dtype)
-        if col in pref_dict_num:
-            cols_to_be_dropped.append(col+'_range')
-    # convert_dtype_dict['listing_id'] = 'int32'
-    # print(convert_dtype_dict)
-    df_user_row = df_user_row.astype(convert_dtype_dict)
-    return df_user_row, cols_to_be_dropped
 
 def get_top_recommendations_user_item(similarity_user_item, ind_ordered, x_items, y_items, k, df_prepared_final):
     similarity_user_item_ordered = similarity_user_item[ind_ordered]
-    x_items_ordered = x_items[ind_ordered]
-    y_items_ordered = y_items[ind_ordered]
-    x_top_k = x_items_ordered[:k]
-    count = 0
-    per_num = 100
-    while count < k:
-        per_num = per_num - 1
-        percentile = np.percentile(similarity_user_item, per_num)
-        count = (similarity_user_item_ordered > percentile).sum()
-
-    # print('ind_ordered: ', ind_ordered)
-    sample_similarity_user_item_ordered = similarity_user_item_ordered[:count]
-    sample_indices = ind_ordered[:count]
-    sample_prob = sample_similarity_user_item_ordered / np.sum(sample_similarity_user_item_ordered)
-    topk_indices = np.random.choice(sample_indices, size=k, replace=False, p=sample_prob)
-    # print('topk_indices: ', topk_indices)
+#     x_items_ordered = x_items[ind_ordered]
+#     y_items_ordered = y_items[ind_ordered]
+    similarity_user_item_ordered = similarity_user_item[ind_ordered]
+    topk_indices = ind_ordered[:k]
     df_result = df_prepared_final.iloc[topk_indices,:]
-
+    # print('similarity_user_item_ordered: ', similarity_user_item_ordered)
     return df_result
+
+def prepare_df_user_row(df_one_hot, my_dict, user_pref_dict):
+    user_row ={}
+    df_user_row = pd.DataFrame(columns = df_one_hot.columns)
+    convert_dtype_dict = {}
+    for col in df_one_hot.columns:
+        convert_dtype_dict[col] = str(df_one_hot[col].dtype)
+        if col in my_dict:
+            user_row[col+'_'+user_pref_dict[col]] = 1
+        elif col in user_pref_dict.values():
+            user_row[col] = 1
+    df_user_row = df_user_row.append(user_row,ignore_index=True)
+    df_user_row = df_user_row.fillna(0)
+    df_user_row = df_user_row.astype(convert_dtype_dict)
+    return df_user_row
+
+# def get_top_recommendations_user_item_with_sampling(similarity_user_item, ind_ordered, x_items, y_items, k, df_prepared_final):
+#     similarity_user_item_ordered = similarity_user_item[ind_ordered]
+#     x_items_ordered = x_items[ind_ordered]
+#     y_items_ordered = y_items[ind_ordered]
+#     similarity_user_item_ordered = similarity_user_item[ind_ordered]
+#     x_top_k = x_items_ordered[:k]
+#     count = 0
+#     per_num = 100
+# #     print('np.percentile(similarity_user_item, per_num): ', np.percentile(similarity_user_item, per_num))
+#     while count < k:
+#         per_num = per_num - 1
+#         percentile = np.percentile(similarity_user_item, per_num)
+#         count = (similarity_user_item_ordered > percentile).sum()
+#
+#     print('count: ', count)
+#     print('ind_ordered: ', ind_ordered)
+# #     print('similarity_user_item_ordered[:count]: ', similarity_user_item_ordered[:count])
+#
+#     sample_similarity_user_item_ordered = similarity_user_item_ordered[:count]
+#     sample_indices = ind_ordered[:count]
+#     sample_prob = sample_similarity_user_item_ordered / np.sum(sample_similarity_user_item_ordered)
+# #     print('sample_prob: ', sample_prob)
+#     topk_indices = np.random.choice(sample_indices, size=k, replace=False, p=sample_prob)
+#     print('topk_indices: ', topk_indices)
+#     df_result = df_prepared_final.iloc[topk_indices,:]
+# #     df_result = df_prepared_final.iloc[ind_ordered[:k],:]
+#
+#     return df_result
